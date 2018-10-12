09a40217dc40bc6c1e91a7c7464862d2970a99c7
archangel <archangel@spire.attlocal.net>
2018-10-11 20:10:54 -0500

    minor margin adjustment

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ const styles = {
     boxShadow: '0px 12px 13px 10px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
     marginTop: '0px',
     marginBottom: '30px',
-    marginLeft: '30px',
-    marginRight: '30px',
+    marginLeft: '90px',
+    marginRight: '90px',
   },
   visibleView: {
     paddingBottom: '60px',
